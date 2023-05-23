9b6fdbdcec98fab457b13628b3eb8310a5a5c74a
wfusiman <wfusiman@gmail.com>
2023-05-23 00:55:39 -0300

    update token

--- frontend/src/app/services/esriConfig.ts
+++ frontend/src/app/services/esriConfig.ts
@@ -1,5 +1,5 @@
 export const ESRI_PARAMS = {
     PUERTO_MADRYN: [-42.775935, -65.038144],
     apiKey: 'AAPK584039e30f704235ab1574cf4c54fd21QZz2aOQkcy5ZtwcrYir3b25zM23_TALwKka8f7IHtzUgsZLkzHhndEr6lv3CtESm',
-    token: 'Owdz4mR_KuiGFDbt5TJTKmgs4G5M9yjJM-83pD5R6GPwsqHXUhyWywTPIJLLkFsrsQueAZulTarUbjLonBm1P9Q0Yg44cr-Xk39-2ompbSpi_B60ntvvPDao9dVVmm2-EHntWw4LMfF2L-x1U7N_Qw..',
+    token: 'vd33Qo_tqpeJ8xU8Hr1kdXN1PSaPO3irSI-RyNco7yglKe2pduT-rf9DoRqjxlGL-7bisuCQ34sN7v3FIFp6wIOtUXtkBkVoPq0rzyl34ca5wL1Re6OETcH9b5aLMjw0S7jmIcGNRQcmChkcdDdbuw..',
 }
