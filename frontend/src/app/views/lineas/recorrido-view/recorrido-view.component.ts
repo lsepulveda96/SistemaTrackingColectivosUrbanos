cf032ae82276878d11362938861238d18e40d408
wfusiman <wfusiman@gmail.com>
2023-06-27 02:13:23 -0300

    opcion ver paradas en recorrido

--- frontend/src/app/views/lineas/recorrido-view/recorrido-view.component.ts
+++ frontend/src/app/views/lineas/recorrido-view/recorrido-view.component.ts
@@ -19,9 +19,11 @@ export class RecorridoViewComponent implements OnInit {
   linea: Linea;
   recorridos: any[]; // recorridos activos
   map: any;
-  recGroup: any;
+  recGroup: any; // grupo de trayectos del recorrido actual seleccionado.
+  paradasGroup: any; // grupo de paradas del recorrido actual seleccionado.
   recorridoIC = new FormControl(null);
   viewParadas: boolean = false;
+  showParadasIC = new FormControl(false);
 
   colors = ['#008000', '#0000ff', '#ffa500', '#a52a2a', '#8b008b', '#1e90ff'];
 
@@ -81,6 +83,10 @@ export class RecorridoViewComponent implements OnInit {
   }
 
   onSelectRecorrido() {
+    this.showParadasIC.setValue(false);
+    if (this.paradasGroup)
+      this.paradasGroup.clearLayers();
+
     // Toma el recorrido seleccionado
     const recSel = this.recorridoIC.value[0];
     if (this.recGroup) // elimina todos los layers en el grupo
@@ -98,6 +104,44 @@ export class RecorridoViewComponent implements OnInit {
     }
     this.recGroup.addLayer(polyline);
     this.recGroup.addTo(this.map);
+  }
+
+  changeShowParadas(event: any) {
+    if (this.showParadasIC.value && this.recorridoIC.value) {
+      const recSel = this.recorridoIC.value[0];
+      if (!recSel.paradas) {
+        this.waiting = true;
+        this.serviceLinea.getParadasRecorrido( recSel.id ).subscribe( result => {
+          this.waiting = false;
+          if (!result.error) {
+            recSel.paradas = result.data;
+            this.showParadasRecorrido( recSel.paradas );
+          }
+        });
+      }
+      else {
+        this.showParadasRecorrido( recSel.paradas );
+      }
+    }
+    else if (!this.showParadasIC.value && this.paradasGroup) {
+      this.paradasGroup.clearLayers();
+    }
+  }
+
+  showParadasRecorrido( paradasRec: any[] ) {
+    if (this.paradasGroup) 
+      this.paradasGroup.clearLayers();
+    else
+      this.paradasGroup = new L.LayerGroup();
+
+    for (let paradaRec of paradasRec ) {
+      const mark = new L.Marker( L.latLng( paradaRec.parada.coordenada.lat, paradaRec.parada.coordenada.lng ) );
+      const begin_end = paradaRec.orden == 0 ? 'INICIO ' : (paradaRec.orden == paradasRec.length-1 ? 'FIN ': ''); 
+      mark.bindPopup(begin_end + paradaRec.parada.codigo + ': ' + paradaRec.parada.direccion,
+        { closeOnClick: true, autoClose: true });
+      this.paradasGroup.addLayer( mark );
+    }
+    this.paradasGroup.addTo(this.map);
   }
 
   nuevoRecorrido() {
