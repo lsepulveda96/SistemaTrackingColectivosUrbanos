e8921c6b083c8c4a64ea5489bce4ef6b25661dba
Waldo Fusiman <wfusiman@gmail.com>
2023-12-26 22:21:48 -0300

    genera script de empaquetamiento del backend y frontend

---
+++ build-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ruta relativa desde el home del usuario hacia el directorio del proyecto.
+D_PROJECT=/Escritorio/stcu/stcu2
+# ruta completa del proyecto
+D_BASE=$HOME$D_PROJECT
+# ruta a la carpeta destino de los builds
+D_TARGET=$HOME/shared
+
+# Ejecuta build del backend y una vez finalizado copiar archivo war al directorio shared.
+echo -e "\n---------------------------------------------------------------\n-------------- Building backend...\n---------------------------------------------------------------\n"
+cd $D_BASE/backend
+mvn clean package 
+echo -e "\n---------------------------------------------------------------\n-------------- Building backend finished\n---------------------------------------------------------------\n"
+echo -e "\n---------------------------------------------------------------\n-------------- Copy war to user directory $D_TARGET\n---------------------------------------------------------------\n"
+cp ./target/stcu2service.war $D_TARGET
+echo -e "\n---------------------------------------------------------------\n-------------- Copy war finished \n---------------------------------------------------------------\n"
+echo -e "\n---------------------------------------------------------------\n-------------- Building frontend...\n---------------------------------------------------------------\n"
+cd $D_BASE/frontend
+npm run build
+echo -e "\n---------------------------------------------------------------\n-------------- Building frontend finished\n---------------------------------------------------------------\n"
+echo -e "\n---------------------------------------------------------------\n-------------- Copying dist to user directory $D_TARGET\n---------------------------------------------------------------\n"
+cp -r dist $D_TARGET
+echo -e "\n---------------------------------------------------------------\n------------- Copy dist finished ------------------\n---------------------------------------------------------------\n" 
+
+
+echo -e "\n---------------------------------------------------------------\n------------- Building back front have completed.\n---------------------------------------------------------------\n"
